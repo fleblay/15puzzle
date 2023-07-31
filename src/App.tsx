2fc3a483ec68d846593e2cc9e3bc5f84f3d43216
Frederic Le Blay <frederic.leblay@gmail.com>
2023-07-31 23:40:51 +0200

    fonctionnal version grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
-import { Grid } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
+import { SxProps } from "@mui/material/styles";
+
+const commonStyles: SxProps = {
+	border: 2,
+	borderRadius: 1,
+	height: "20vh",
+	margin: 0.5,
+}
 
 function Board() {
+	const cells: React.ReactElement[] = []
+	for (let j = 0; j < 4; j++) {
+		cells.push(
+			<Grid container>
+				<Grid item xs={3} >
+					<Box sx={{ ...commonStyles }}>
+					</Box>
+				</Grid>
+				<Grid item xs={3} >
+					<Box sx={{ ...commonStyles, }}>
+					</Box>
+				</Grid>
+				<Grid item xs={3} >
+					<Box sx={{ ...commonStyles }}>
+					</Box>
+				</Grid>
+				<Grid item xs={3} >
+					<Box sx={{ ...commonStyles }}>
+					</Box>
+				</Grid>
+			</Grid >
+		)
+	}
 	return (
-		<Grid container spacing={2} sx={{ margin: "auto" , border : "solid"}}>
-			<Grid item xs={8} sx={{ border: "solid", borderColor: "blue", borderRadius: "20px" }}>
-				<p>xs=8</p>
-			</Grid>
-			<Grid item xs={4}>
-				<p>xs=4</p>
-			</Grid>
-			<Grid item xs={4}>
-				<p>xs=4</p>
-			</Grid>
-			<Grid item xs={8}>
-				<p>xs=8</p>
-			</Grid>
+		<Grid sx={{ margin: "auto", maxWidth: 600}}>
+			{cells}
 		</Grid>
 	)
 }
